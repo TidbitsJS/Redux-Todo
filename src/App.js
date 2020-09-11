b92d9d3fc37c5a9dd583d7e24c15b577b8888e48
sujata <gunalesujata@gmail.com>
2020-09-12 01:06:00 +0530

    redux dev tool

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import "./App.css";
 import Todo from "./components/Todo";
 import reducer from "./config/Reducer";
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 function App() {
   return (
